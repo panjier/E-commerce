bba1e395f414b5406787016b593efcd8dbe6325d
Panji Erlangga <panji@aplikasix.dev>
2023-08-31 11:41:39 +0700

    add content shop

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const shop = () => {
-  return <div>shop</div>;
+  return (
+    <>
+      <section className='shop-banner p-2'>
+        <div className='container-xxl'>
+          <div className='row '>
+            <div className='repair-details text-center'>
+              <h1 className='text-white mt-2'>
+                <span className='text-success'>100%</span> Off On All Product
+                And Accessories
+              </h1>
+            </div>
+          </div>
+          <p className='text-white text-center'>
+            Make Your Orders We Will Deliver
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default shop;
